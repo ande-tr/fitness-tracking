4a9d9b2b93ed0a45f35f212dd05a62cd44bf8237
Andreea <andreea.trufan@student.howest.be>
2024-01-16 17:08:04 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,321 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Dimensions, Platform } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import Button from './components/Button'; 
+import ImageViewer from './components/ImageViewer';
+import { useState, useEffect, useRef } from 'react';
+import * as ScreenOrientation from 'expo-screen-orientation';
+import { ExpoWebGLRenderingContext } from 'expo-gl';
+import { Camera } from 'expo-camera';
+import {
+  cameraWithTensors,
+} from '@tensorflow/tfjs-react-native';
+
+import * as tf from "@tensorflow/tfjs"
+import "@tensorflow/tfjs-react-native"
+import * as poseDetection from '@tensorflow-models/pose-detection';
+
+import { CameraType } from 'expo-camera/build/Camera.types';
+
+const PlaceholderImage = require("./assets/images/background-image.png");
+
+const TensorCamera = cameraWithTensors(Camera);
+
+const IS_ANDROID = Platform.OS === 'android';
+const IS_IOS = Platform.OS === 'ios';
+const CAM_PREVIEW_WIDTH = Dimensions.get('window').width;
+const CAM_PREVIEW_HEIGHT = CAM_PREVIEW_WIDTH / (IS_IOS ? 9 / 16 : 3 / 4);
+const MIN_KEYPOINT_SCORE = 0.3;
+const OUTPUT_TENSOR_WIDTH = 180;
+const OUTPUT_TENSOR_HEIGHT = OUTPUT_TENSOR_WIDTH / (IS_IOS ? 9 / 16 : 3 / 4);
+const AUTO_RENDER = false;
+const LOAD_MODEL_FROM_BUNDLE = false;
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [isTfReady, setIsTfReady] = useState(null);
+  const [poses, setPoses] = useState(null);
+  const [fps, setFps] = useState(0);
+  const [orientation, setOrientation] = useState(null);
+  const [cameraType, setCameraType] = useState(
+    Camera.Constants.Type.front
   );
+  const [model, setModel] = useState(null);
+
+  const rafId = useRef(null);
+
+  const cameraRef = useRef(null);
+
+  const TensorCamera = cameraWithTensors(Camera);
+
+  const IS_ANDROID = Platform.OS === 'android';
+  const IS_IOS = Platform.OS === 'ios';
+
+  let detector;
+
+  // const waitForTf = async () => {
+  //   try {
+  //     await tf.ready();
+  //     setIsTfReady(true);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+
+  const setPoseModel = async () => {
+    model = poseDetection.SupportedModels.MoveNet;
+    detector = await poseDetection.createDetector(model).then((response) => console.log(response));
+  }
+
+  const setUpApp = async () => {
+    rafId.current = null;
+
+    const curOrientation = await ScreenOrientation.getOrientationAsync();
+    setOrientation(curOrientation);
+
+    ScreenOrientation.addOrientationChangeListener((event) => {
+      setOrientation(event.orientationInfo.orientation);
+    });
+
+    await Camera.requestCameraPermissionsAsync();
+    await tf.ready();
+
+    const movenetModelConfig = {
+      modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING,
+      enableSmoothing: true,
+    };
+
+    const model = await poseDetection.SupportedModels.MoveNet;
+    detector = await poseDetection.createDetector(model, movenetModelConfig).then((response) => console.log(response));
+
+    setModel(detector);
+    setIsTfReady(true);
+  }
+
+  useEffect(() => {
+    setUpApp();
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      if (rafId.current != null && rafId.current !== 0) {
+        cancelAnimationFrame(rafId.current);
+        rafId.current = 0;
+      }
+    };
+  }, []);
+
+  const handleCameraStream = async (
+    images,
+    updatePreview,
+    gl
+  ) => {
+    const loop = async () => {
+      // Get the tensor and run pose detection.
+      const imageTensor = images.next().value;
+
+      const startTs = Date.now();
+      const poses = await model.estimatePoses(
+        imageTensor,
+        undefined,
+        Date.now()
+      );
+      const latency = Date.now() - startTs;
+      setFps(Math.floor(1000 / latency));
+      setPoses(poses);
+      tf.dispose([imageTensor]);
+
+      if (rafId.current === 0) {
+        return;
+      }
+
+      if (!AUTO_RENDER) {
+        updatePreview();
+        gl.endFrameEXP();
+      }
+
+      rafId.current = requestAnimationFrame(loop);
+    };
+
+    loop();
+  };
+
+  // const pickImageAsync = async () => {
+  //   let result = await ImagePicker.launchImageLibraryAsync({
+  //     allowsEditing: true,
+  //     quality: 1,
+  //   });
+
+  //   if (!result.canceled) {
+  //     setSelectedImage(result.assets[0].uri);
+  //   } else {
+  //     alert('You did not select any image.');
+  //   }
+  // };
+
+  // componentDidMount() {
+  //   this.setupConnection();
+  // }
+
+  const renderPose = () => {
+    if (poses != null && poses.length > 0) {
+      const keypoints = poses[0].keypoints
+        .filter((k) => (k.score ?? 0) > MIN_KEYPOINT_SCORE)
+        .map((k) => {
+          // Flip horizontally on android or when using back camera on iOS.
+          const flipX = IS_ANDROID || cameraType === Camera.Constants.Type.back;
+          const x = flipX ? getOutputTensorWidth() - k.x : k.x;
+          const y = k.y;
+          const cx =
+            (x / getOutputTensorWidth()) *
+            (isPortrait() ? CAM_PREVIEW_WIDTH : CAM_PREVIEW_HEIGHT);
+          const cy =
+            (y / getOutputTensorHeight()) *
+            (isPortrait() ? CAM_PREVIEW_HEIGHT : CAM_PREVIEW_WIDTH);
+          return (
+            <Circle
+              key={`skeletonkp_${k.name}`}
+              cx={cx}
+              cy={cy}
+              r='4'
+              strokeWidth='2'
+              fill='#00AA00'
+              stroke='white'
+            />
+          );
+        });
+
+      return <Svg style={styles.svg}>{keypoints}</Svg>;
+    } else {
+      return <View></View>;
+    }
+  };
+
+  const renderFps = () => {
+    return (
+      <View style={styles.fpsContainer}>
+        <Text>FPS: {fps}</Text>
+      </View>
+    );
+  };
+
+  const renderCameraTypeSwitcher = () => {
+    return (
+      <View
+        style={styles.cameraTypeSwitcher}
+        onTouchEnd={handleSwitchCameraType}
+      >
+        <Text>
+          Switch to{' '}
+          {cameraType === Camera.Constants.Type.front ? 'back' : 'front'} camera
+        </Text>
+      </View>
+    );
+  };
+
+  const handleSwitchCameraType = () => {
+    if (cameraType === Camera.Constants.Type.front) {
+      setCameraType(Camera.Constants.Type.back);
+    } else {
+      setCameraType(Camera.Constants.Type.front);
+    }
+  };
+
+  const isPortrait = () => {
+    return (
+      orientation === ScreenOrientation.Orientation.PORTRAIT_UP ||
+      orientation === ScreenOrientation.Orientation.PORTRAIT_DOWN
+    );
+  };
+
+  const getOutputTensorWidth = () => {
+    // On iOS landscape mode, switch width and height of the output tensor to
+    // get better result. Without this, the image stored in the output tensor
+    // would be stretched too much.
+    //
+    // Same for getOutputTensorHeight below.
+    return isPortrait() || IS_ANDROID
+      ? OUTPUT_TENSOR_WIDTH
+      : OUTPUT_TENSOR_HEIGHT;
+  };
+
+  const getOutputTensorHeight = () => {
+    return isPortrait() || IS_ANDROID
+      ? OUTPUT_TENSOR_HEIGHT
+      : OUTPUT_TENSOR_WIDTH;
+  };
+
+  const getTextureRotationAngleInDegrees = () => {
+    // On Android, the camera texture will rotate behind the scene as the phone
+    // changes orientation, so we don't need to rotate it in TensorCamera.
+    if (IS_ANDROID) {
+      return 0;
+    }
+
+    // For iOS, the camera texture won't rotate automatically. Calculate the
+    // rotation angles here which will be passed to TensorCamera to rotate it
+    // internally.
+    switch (orientation) {
+      // Not supported on iOS as of 11/2021, but add it here just in case.
+      case ScreenOrientation.Orientation.PORTRAIT_DOWN:
+        return 180;
+      case ScreenOrientation.Orientation.LANDSCAPE_LEFT:
+        return cameraType === Camera.Constants.Type.front ? 270 : 90;
+      case ScreenOrientation.Orientation.LANDSCAPE_RIGHT:
+        return cameraType === Camera.Constants.Type.front ? 90 : 270;
+      default:
+        return 0;
+    }
+  };
+
+  if (!isTfReady) {
+    return (
+      <View style={styles.loadingMsg}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  } else {
+    return (
+      // Note that you don't need to specify `cameraTextureWidth` and
+      // `cameraTextureHeight` prop in `TensorCamera` below.
+      <View
+        style={
+          isPortrait() ? styles.containerPortrait : styles.containerLandscape
+        }
+      >
+        <TensorCamera
+          ref={cameraRef}
+          style={styles.camera}
+          autorender={AUTO_RENDER}
+          type={cameraType}
+          // tensor related props
+          resizeWidth={getOutputTensorWidth()}
+          resizeHeight={getOutputTensorHeight()}
+          resizeDepth={3}
+          rotation={getTextureRotationAngleInDegrees()}
+          onReady={handleCameraStream}
+        />
+        {renderPose()}
+        {renderFps()}
+        {renderCameraTypeSwitcher()}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
